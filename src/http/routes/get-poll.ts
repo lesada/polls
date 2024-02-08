bfcad3e5c69e11bac332255d499ecdeaba495eb0
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2024-02-08 12:55:30 -0300

    feat: redis

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import z from "zod";
 import { prisma } from "../../lib/prisma";
+import { redis } from "../../lib/redis";
 
 export async function getPoll(app: FastifyInstance) {
   app.get("/polls/:pollId", async (request, reply) => {
@@ -24,8 +25,35 @@ export async function getPoll(app: FastifyInstance) {
       },
     });
 
+    if (!poll) {
+      return reply.status(404).send({
+        error: "Poll not found.",
+      });
+    }
+
+    const result = await redis.zrange(pollId, 0, -1, "WITHSCORES");
+
+    const votes = result.reduce((obj, line, index) => {
+      if (index % 2 === 0) {
+        const score = Number(result[index + 1]);
+
+        Object.assign(obj, {
+          [line]: score,
+        });
+      }
+      return obj;
+    }, {} as Record<string, number>);
+
     return reply.send({
-      poll,
+      poll: {
+        id: poll.id,
+        title: poll.title,
+        options: poll.options.map((option) => ({
+          id: option.id,
+          title: option.title,
+          score: votes[option.id] || 0,
+        })),
+      },
     });
   });
 }
--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import { randomUUID } from "crypto";
 import { FastifyInstance } from "fastify";
 import z from "zod";
 import { prisma } from "../../lib/prisma";
+import { redis } from "../../lib/redis";
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post("/polls/:pollId/votes", async (request, reply) => {
@@ -34,6 +35,7 @@ export async function voteOnPoll(app: FastifyInstance) {
             id: userPreviousVote.id,
           },
         });
+        await redis.zincrby(pollId, -1, userPreviousVote.pollOptionId);
       }
 
       if (userPreviousVote && userPreviousVote.pollOptionId === pollOptionId) {
@@ -60,6 +62,8 @@ export async function voteOnPoll(app: FastifyInstance) {
         pollOptionId,
       },
     });
+
+    await redis.zincrby(pollId, 1, pollOptionId);
 
     return reply.status(201).send();
   });
