05bf9accc714625aea768272aaea1acd98918f86
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2024-02-08 13:35:06 -0300

    feat: websocket

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -3,6 +3,7 @@ import { FastifyInstance } from "fastify";
 import z from "zod";
 import { prisma } from "../../lib/prisma";
 import { redis } from "../../lib/redis";
+import { voting } from "../../utils/voting-pub-sub";
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post("/polls/:pollId/votes", async (request, reply) => {
@@ -35,7 +36,16 @@ export async function voteOnPoll(app: FastifyInstance) {
             id: userPreviousVote.id,
           },
         });
-        await redis.zincrby(pollId, -1, userPreviousVote.pollOptionId);
+        const votes = await redis.zincrby(
+          pollId,
+          -1,
+          userPreviousVote.pollOptionId
+        );
+
+        voting.publish(pollId, {
+          pollOptionId: userPreviousVote.pollOptionId,
+          votes: Number(votes),
+        });
       }
 
       if (userPreviousVote && userPreviousVote.pollOptionId === pollOptionId) {
@@ -63,7 +73,12 @@ export async function voteOnPoll(app: FastifyInstance) {
       },
     });
 
-    await redis.zincrby(pollId, 1, pollOptionId);
+    const votes = await redis.zincrby(pollId, 1, pollOptionId);
+
+    voting.publish(pollId, {
+      pollOptionId,
+      votes: Number(votes),
+    });
 
     return reply.status(201).send();
   });
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,14 @@
 import fastifyCookie from "@fastify/cookie";
+import websocket from "@fastify/websocket";
 import fastify from "fastify";
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
 import { voteOnPoll } from "./routes/vote-on-poll";
+import { pollResults } from "./ws/poll-results";
 
 const app = fastify();
+
+app.register(websocket);
 
 app.register(fastifyCookie, {
   secret: "hellodarknessmyoldfriend",
@@ -14,6 +18,7 @@ app.register(fastifyCookie, {
 app.register(createPoll);
 app.register(getPoll);
 app.register(voteOnPoll);
+app.register(pollResults);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running on port 3333!");
